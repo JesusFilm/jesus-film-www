0849d04156ee0c614049245838aa6cbd4a148617
David Jensen <david@dkjensen.com>
2022-02-07 16:47:08 -0500

    fix: build asset path

--- bin/build-zip.sh
+++ bin/build-zip.sh
@@ -11,13 +11,12 @@ mkdir -p "$DEST_PATH"
 
 echo "Installing PHP and JS dependencies..."
 npm ci
-composer install || exit "$?"
+composer clear-cache
+composer install --no-dev --prefer-dist || exit "$?"
 echo "Running JS Build..."
 npm run build || exit "$?"
 echo "Generating translations..."
 npm run i18n || exit "$?"
-echo "Cleaning up PHP dependencies..."
-composer install --no-dev || exit "$?"
 
 echo "Syncing files..."
 rsync -rc --exclude-from="$PROJECT_PATH/.distignore" "$PROJECT_PATH/" "$DEST_PATH/" --delete --delete-excluded
