75e853633b0192b906b87ef6a43f83e9724e4495
David Jensen <david@dkjensen.com>
2022-02-07 17:39:13 -0500

    feat: update deploy script

--- .releaserc.js
+++ .releaserc.js
@@ -3,23 +3,22 @@ module.exports = {
   branch: "master",
   plugins: [
     "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/changelog",
-      {
-        "changelogFile": "changelog.txt"
-      }
-    ],
-    ["@semantic-release/npm", { npmPublish: false }],
-    [
-      "semantic-release-plugin-update-version-in-files",
-      {
-        "files": [
-          "package.json",
-          "style.css"
-        ],
-        "placeholder": "0.0.0-development"
-      }
-    ],
+    ["@semantic-release/npm", { 
+      npmPublish: false 
+    }],
+    // [
+    //   "semantic-release-plugin-update-version-in-files",
+    //   {
+    //     "files": [
+    //       "package.json",
+    //       "style.css"
+    //     ],
+    //     "placeholder": "0.0.0-development"
+    //   }
+    // ],
+    ["@semantic-release/exec", {
+      "publishCmd": "./bin/build-zip.sh ${nextRelease.version}"
+    }],
     ["@semantic-release/github", {
       "assets": [
         {"path": "build/jesus-film-www.zip", "label": "Bundled theme"}
