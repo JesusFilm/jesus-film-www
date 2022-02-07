19f0439f2e1fb0baf068c39572d3edbb1991047d
David Jensen <david@dkjensen.com>
2022-02-07 17:58:02 -0500

    fix: .releaserc.js

--- .releaserc.js
+++ .releaserc.js
@@ -2,6 +2,7 @@ module.exports = {
   tagFormat: "${version}",
   branch: "master",
   plugins: [
+    "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     ["@semantic-release/npm", { 
       npmPublish: false 
