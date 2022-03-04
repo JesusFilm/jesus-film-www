3931d1d1a0136d921c1921b714e29f15a678e470
David Jensen <david@dkjensen.com>
2022-03-04 08:08:48 -0500

    feat: add group width control

--- resources/js/editor.js
+++ resources/js/editor.js
@@ -2,11 +2,13 @@ const { registerPlugin } = wp.plugins;
 const { __ } = wp.i18n;
 const { useSelect, dispatch } = wp.data;
 const { Fragment } = wp.element; // eslint-disable-line
+const { createHigherOrderComponent } = wp.compose; // eslint-disable-line
 const { PluginDocumentSettingPanel } = wp.editPost;  // eslint-disable-line
-const { SelectControl, ToggleControl, PanelBody } = wp.components; // eslint-disable-line
+const { SelectControl, ToggleControl, PanelBody, __experimentalUnitControl: UnitControl } = wp.components; // eslint-disable-line
 const { InspectorControls, BlockEdit } = wp.blockEditor; // eslint-disable-line
 
 import './blocks';
+import classNames from 'classnames';
 
 wp.domReady( () => {
 	wp.blocks.registerBlockStyle( 'core/cover', {
@@ -72,7 +74,7 @@ registerPlugin( 'jf-submenu-panel', {
  */
 wp.hooks.addFilter(
 	'blocks.registerBlockType',
-	'moneythink/columns-custom-attribute',
+	'jesusfilm/columns-custom-attribute',
 	function( settings, name ) {
 		if ( typeof settings.attributes !== 'undefined' ) {
 			if ( name === 'core/columns' ) {
@@ -112,13 +114,13 @@ const columnsControls = wp.compose.createHigherOrderComponent( ( BlockEdit ) => 
 
 wp.hooks.addFilter(
 	'editor.BlockEdit',
-	'moneythink/columns-reverse-mobile-control',
+	'jesusfilm/columns-reverse-mobile-control',
 	columnsControls,
 );
 
 wp.hooks.addFilter(
 	'blocks.getSaveContent.extraProps',
-	'moneythink/columns-apply-class',
+	'jesusfilm/columns-apply-class',
 	function( extraProps, blockType, attributes ) {
 		const { reverseColumnsMobile } = attributes;
 
@@ -127,4 +129,143 @@ wp.hooks.addFilter(
 		}
 		return extraProps;
 	},
+);
+
+// Enable custom attributes on Image block
+const enableWidthSelectOnBlocks = [
+	'core/group',
+];
+
+/**
+ * Declare our custom attribute
+ *
+ * @param  settings
+ * @param  name
+ */
+const setWidthAttribute = ( settings, name ) => {
+	// Do nothing if it's another block than our defined ones.
+	if ( ! enableWidthSelectOnBlocks.includes( name ) ) {
+		return settings;
+	}
+
+	return Object.assign( {}, settings, {
+		attributes: Object.assign( {}, settings.attributes, {
+			groupWidth: { type: 'string' },
+		} ),
+	} );
+};
+wp.hooks.addFilter(
+	'blocks.registerBlockType',
+	'jesusfilm/set-sidebar-select-attribute',
+	setWidthAttribute,
+);
+
+/**
+ * Add Custom Select to Image Sidebar
+ */
+const withWidthSelect = createHigherOrderComponent( ( BlockEdit ) => {
+	return ( props ) => {
+		// If current block is not allowed
+    	if ( ! enableWidthSelectOnBlocks.includes( props.name ) ) {
+			return (
+				<BlockEdit { ...props } />
+			);
+		}
+
+		const { attributes, setAttributes } = props;
+		const { groupWidth } = attributes;
+
+		return (
+			<Fragment>
+				<BlockEdit { ...props } />
+				<InspectorControls>
+                	<PanelBody
+    	                title={ __( 'Sizing' ) }
+    	            >
+
+						<UnitControl
+							label={ __( 'Width' ) }
+							onChange={ ( value ) => {
+								setAttributes( {
+									groupWidth: value,
+								} );
+							} }
+							value={ groupWidth }
+						/>
+
+	                </PanelBody>
+				</InspectorControls>
+			</Fragment>
+		);
+	};
+}, 'withWidthSelect' );
+
+wp.hooks.addFilter(
+	'editor.BlockEdit',
+	'jesusfilm/with-sidebar-select',
+	withWidthSelect,
+);
+
+/**
+ * Add custom class to block in Edit
+ */
+const withWidthSelectProp = createHigherOrderComponent( ( BlockListBlock ) => {
+	return ( props ) => {
+		// If current block is not allowed
+		if ( ! enableWidthSelectOnBlocks.includes( props.name ) ) {
+			return (
+				<BlockListBlock { ...props } />
+			);
+		}
+
+		const { attributes } = props;
+		const { groupWidth } = attributes;
+
+		if ( groupWidth ) {
+			let wrapperProps = props.wrapperProps;
+
+			wrapperProps = {
+				...wrapperProps,
+				style: {
+					...( wrapperProps && { ...wrapperProps.style } ),
+					maxWidth: groupWidth,
+				},
+			};
+
+			return <BlockListBlock { ...props } className={ 'has-max-width' } wrapperProps={ wrapperProps } />;
+		}
+		return <BlockListBlock { ...props } />;
+	};
+}, 'withWidthSelectProp' );
+
+wp.hooks.addFilter(
+	'editor.BlockListBlock',
+	'jesusfilm/with-sidebar-select-prop',
+	withWidthSelectProp,
+);
+
+/**
+ * Save our custom attribute
+ *
+ * @param  props
+ * @param  blockType
+ * @param  attributes
+ */
+const saveWidthAttribute = ( props, blockType, attributes ) => {
+	// Do nothing if it's another block than our defined ones.
+	if ( enableWidthSelectOnBlocks.includes( blockType.name ) ) {
+		const { groupWidth } = attributes;
+		if ( groupWidth ) {
+			props.className = classNames( props.className, 'has-max-width' );
+
+			Object.assign( props, { style: { ...props.style, maxWidth: groupWidth } } );
+		}
+	}
+
+	return props;
+};
+wp.hooks.addFilter(
+	'blocks.getSaveContent.extraProps',
+	'jesusfilm/save-sidebar-select-attribute',
+	saveWidthAttribute,
 );
